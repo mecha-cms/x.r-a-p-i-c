1321ba267e2004a2d43a413f69c5a059c73aef1f
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-01-12 00:48:04 +0700

    Update Hooks

--- r-a-p-i-c/index__.php
+++ r-a-p-i-c/index__.php
@@ -1,14 +1,14 @@
 <?php
 
-function fn_rapic($data) {
-    if (!empty($data['content']) && (!isset($data['rapic']) || $data['rapic'])) {
-        $p = explode('</p>', $data['content']);
-        ob_start();
-        include __DIR__ . DS . 'lot' . DS . 'worker' . DS . 'content.php';
-        array_splice($p, array_rand($p), 0, ob_get_clean() . X);
-        $data['content'] = str_replace([X . '</p>', X], "", implode('</p>', $p));
+function fn_rapic($content, $lot) {
+    if (isset($lot['rapic']) && !$lot['rapic']) {
+        return $content;
     }
-    return $data;
+    $p = explode('</p>', $content);
+    ob_start();
+    include __DIR__ . DS . 'lot' . DS . 'worker' . DS . 'content.php';
+    array_splice($p, array_rand($p), 0, ob_get_clean() . X);
+    return str_replace([X . '</p>', X], "", implode('</p>', $p));
 }
 
-Hook::set('page.output', 'fn_rapic');
+Hook::set('page.content', 'fn_rapic');
