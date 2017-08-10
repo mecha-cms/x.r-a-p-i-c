f1c1a18115bd61ff4e149df7319181b1c5cf8b6d
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-08-10 18:30:19 +0700

    Safe Output Buffer

--- r-a-p-i-c/index__.php
+++ r-a-p-i-c/index__.php
@@ -1,13 +1,18 @@
 <?php
 
+ob_start();
+include __DIR__ . DS . 'lot' . DS . 'worker' . DS . 'content.php';
+Lot::set('r_a_p_i_c', ob_get_clean());
+
 function fn_rapic_replace($content, $lot) {
     if (isset($lot['rapic']) && !$lot['rapic']) {
         return $content;
     }
+    if (!isset($lot['path']) || strpos($lot['path'], PAGE) !== 0) {
+        return $content;
+    }
     $p = explode('</p>', $content);
-    ob_start();
-    include __DIR__ . DS . 'lot' . DS . 'worker' . DS . 'content.php';
-    array_splice($p, array_rand($p), 0, ob_get_clean() . X);
+    array_splice($p, array_rand($p), 0, Lot::get('r_a_p_i_c', "") . X);
     return str_replace([X . '</p>', X], "", implode('</p>', $p));
 }
 
