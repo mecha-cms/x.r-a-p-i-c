a1f289929c1c9589c5fc174cfc15bdfbfacec2cc
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-12-29 09:35:19 +0700

    Update index.php

--- r-a-p-i-c/index.php
+++ r-a-p-i-c/index.php
@@ -3,9 +3,12 @@
 ob_start();
 
 function rapic($content, array $lot = []) {
+    global $config;
     if ($this->rapic !== null && !$this->rapic) {
         return $content;
     } else if (!$this->path || strpos($this->path, PAGE . DS) !== 0) {
+        return $content;
+    } else if ($config->is('pages')) {
         return $content;
     }
     $ad = "";
@@ -14,7 +17,7 @@ function rapic($content, array $lot = []) {
         extract(\Lot::get(), EXTR_SKIP);
         include __DIR__ . DS . 'lot' . DS . 'worker' . DS . 'content.php';
         $ad = ob_get_clean();
-    }, [], $this);
+    }, [], $this, \Hook::class);
     $parts = explode('</p>', $content);
     array_splice($parts, array_rand($parts), 0, $ad . X);
     return str_replace([X . '</p>', X], "", implode('</p>', $parts));
