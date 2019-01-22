04e8911b38cc03fc9e909446b12da62e102a14db
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-01-22 18:22:01 +0700

    Micro Optimization

--- r-a-p-i-c/index.php
+++ r-a-p-i-c/index.php
@@ -1,26 +1,24 @@
 <?php namespace fn;
 
-ob_start();
-
 function rapic($content, array $lot = []) {
     global $config;
     if ($this->rapic !== null && !$this->rapic) {
         return $content;
-    } else if (!$this->path || strpos($this->path, PAGE . DS) !== 0) {
+    } else if (!$this->path || \strpos($this->path, PAGE . DS) !== 0) {
         return $content;
     } else if ($config->is('pages')) {
         return $content;
     }
     $ad = "";
     \fn(function() use(&$ad) {
-        ob_start();
-        extract(\Lot::get(), EXTR_SKIP);
+        \ob_start();
+        extract(\Lot::get(), \EXTR_SKIP);
         include __DIR__ . DS . 'lot' . DS . 'worker' . DS . 'content.php';
-        $ad = ob_get_clean();
-    }, [], $this, \Hook::class);
-    $parts = explode('</p>', $content);
-    array_splice($parts, array_rand($parts), 0, $ad . X);
-    return str_replace([X . '</p>', X], "", implode('</p>', $parts));
+        $ad = \ob_get_clean();
+    }, [], $this, \Page::class);
+    $parts = \explode('</p>', $content);
+    \array_splice($parts, \array_rand($parts), 0, $ad . X);
+    return \str_replace([X . '</p>', X], "", \implode('</p>', $parts));
 }
 
 \Hook::set('page.content', __NAMESPACE__ . "\\rapic", 3);
