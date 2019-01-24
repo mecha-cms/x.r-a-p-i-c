411c880386513be6cf172422c433454ea9a60d80
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-01-24 23:09:47 +0700

    Strict Naming
    Move `rapic` Property to `state.r-a-p-i-c`.

--- r-a-p-i-c/index.php
+++ r-a-p-i-c/index.php
@@ -1,24 +1,27 @@
 <?php namespace fn;
 
-function rapic($content, array $lot = []) {
-    global $config;
-    if ($this->rapic !== null && !$this->rapic) {
-        return $content;
-    } else if (!$this->path || \strpos($this->path, PAGE . DS) !== 0) {
-        return $content;
-    } else if ($config->is('pages')) {
+function r_a_p_i_c($content, array $lot = []) {
+    $hash = X . ($path = $this->path) . X;
+    if (!$path || \strpos($path, PAGE . DS) !== 0) {
         return $content;
     }
-    $ad = "";
-    \fn(function() use(&$ad) {
+    $test = $this->get('state.r-a-p-i-c', $hash);
+    if ($test !== $hash && !$test) {
+        return $content;
+    }
+    if (\Config::get('is.pages')) {
+        return $content;
+    }
+    $adv = "";
+    \fn(function() use(&$adv) {
         \ob_start();
         extract(\Lot::get(), \EXTR_SKIP);
         include __DIR__ . DS . 'lot' . DS . 'worker' . DS . 'content.php';
-        $ad = \ob_get_clean();
+        $adv = \ob_get_clean();
     }, [], $this, \Page::class);
     $parts = \explode('</p>', $content);
-    \array_splice($parts, \array_rand($parts), 0, $ad . X);
+    \array_splice($parts, \array_rand($parts), 0, $adv . X);
     return \str_replace([X . '</p>', X], "", \implode('</p>', $parts));
 }
 
-\Hook::set('page.content', __NAMESPACE__ . "\\rapic", 3);
+\Hook::set('page.content', __NAMESPACE__ . "\\r_a_p_i_c", 3);
